0c0f10e5ba2085f18cf5bfcbcf4ac33e492871c8
konojunya <junya03dance@gmail.com>
2018-01-15 12:05:19 +0900

    Add Controller

---
+++ controller/article.go
@@ -0,0 +1,20 @@
+package controller
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func GetArticles(c *gin.Context) {
+}
+
+func GetArticleDetail(c *gin.Context) {
+}
+
+func CreateArticle(c *gin.Context) {
+}
+
+func UpdateArticle(c *gin.Context) {
+}
+
+func DeleteArticle(c *gin.Context) {
+}
