452f868144bfdb12f858aa46c651ec009f659477
konojunya <junya03dance@gmail.com>
2018-02-16 06:55:46 +0900

    CIを少しリッチにする

--- cmd/migration.go
+++ cmd/migration.go
@@ -1,16 +1,23 @@
 package main
 
-import "github.com/konojunya/HEW2018/service"
+import (
+	"log"
+
+	"github.com/konojunya/HEW2018/service"
+)
 
 func main() {
 	err := service.DeleteAllProduct()
 	if err != nil {
 		panic(err)
 	}
+	log.Println("Deleted All Products!")
+
 	for _, product := range service.GetProductsData() {
 		err = service.PostProduct(&product)
 		if err != nil {
 			panic(err)
 		}
+		log.Printf("Delete:\n\tID:%v\n\tTitle:%v", product.ID, product.Title)
 	}
 }
